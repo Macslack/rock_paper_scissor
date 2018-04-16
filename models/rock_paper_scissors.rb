44c416f4ec9691479619eada5ef6252a9ef08dc5
Callum Mackenzie <callum1mackenzie@gmail.com>
2018-04-16 22:00:08 +0100

    still not working

--- models/rock_paper_scissors.rb
+++ models/rock_paper_scissors.rb
@@ -4,13 +4,13 @@ class RockPaperScissors
     hands = [hand1,hand2]
     case hands
       # when hands['rock', 'paper'] || hands['paper', 'rock']
-    when hands[0] = 'rock' && hands[1] = 'paper' || hands[0] = 'paper' && hands[1] = 'rock'
+    when hands[0] == 'rock' && hands[1] == 'paper' || hands[0] == 'paper' && hands[1] == 'rock'
       return "paper beats rock"
       # when hands['scissors', 'paper'] || hands['paper', 'scissors']
-    when hands[0] = 'scissors' && hands[1] = 'paper' || hands[0] = 'paper' && hands[1] ='scissors'
+    when hands[0] == 'scissors' && hands[1] == 'paper' || hands[0] == 'paper' && hands[1] == 'scissors'
       return "scissors beats paper"
       # when hands['rock', 'scissors'] || hands['scissors', 'rock']
-    when hands[0] = 'rock' && hands[1] = 'scissors' || hands[0] = 'scissors' && hand[1] ='rock'
+    when hands[0] == 'rock' && hands[1] == 'scissors' || hands[0] == 'scissors' && hand[1] =='rock'
       return "rock beats scissors"
     return "Draw" unless hands[0] != hands[1]
     end
